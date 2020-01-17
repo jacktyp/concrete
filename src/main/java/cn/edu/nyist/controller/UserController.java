10541081c35dbef10d830f5478a22ae08a0c4ea3
taoyunpeng <taoyunpeng@vortexinfo.cn>
2020-01-17 13:55:11 +0800

    fix

--- src/main/java/cn/edu/nyist/controller/UserController.java
+++ src/main/java/cn/edu/nyist/controller/UserController.java
@@ -7,6 +7,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
@@ -22,7 +23,7 @@ public class UserController {
     @Autowired
     private IUserService userService;
 
-    @RequestMapping("/selectUser")
+    @RequestMapping(value = "/selectUser",method = RequestMethod.GET)
     @ResponseBody
     public String selectUser(Integer id){
         try{
