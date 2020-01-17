dc7834125c6e7aa72616a52b281c60b5bcb3385b
taoyunpeng <taoyunpeng@vortexinfo.cn>
2020-01-17 13:54:32 +0800

    fix

--- src/main/java/cn/edu/nyist/controller/UserController.java
+++ src/main/java/cn/edu/nyist/controller/UserController.java
@@ -2,6 +2,8 @@ package cn.edu.nyist.controller;
 
 import cn.edu.nyist.model.User;
 import cn.edu.nyist.service.IUserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,14 +17,21 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @RequestMapping("/user")
 public class UserController {
+
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
     @Autowired
     private IUserService userService;
 
     @RequestMapping("/selectUser")
     @ResponseBody
     public String selectUser(Integer id){
-        User user = userService.selectUser(id);
-        return user.toString();
+        try{
+            User user = userService.selectUser(id);
+            return user.toString();
+        }catch (Exception e){
+            logger.error("查询失败");
+            return "失败";
+        }
     }
 
 }
